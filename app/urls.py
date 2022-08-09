a8dd5b326ccdc5b4e3edf89686934e58587e3ff0
merwan bekkar <Bkar.mrwan32@gmail.com>
2022-08-09 16:49:44 +0200

    add authentication

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns=[
     path('',views.index,name="index"),
     path('profile/',views.profile,name="profile"),
+    path('signup/',views.signup,name="signup"),
+    path('signin/',views.signin,name="signin"),
+    path('signout/',views.signout,name="signout"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Profile
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 def index(request):
@@ -7,3 +10,34 @@ def index(request):
 
 def profile(request):
     return render(request,'profile.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        if password != cpassword :
+            messages.success(request,"password doesn't match")
+            return render(request,'auth/signup.html')
+        else:
+            user = User.objects.create_user(username=username,email=email,password=password)
+            profil = Profile.objects.create(user=user)
+            if profil :
+                messages.success(request,'profile created please login')
+                return redirect("signin")
+    return render(request,'auth/signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect("profile")
+    return render(request,'auth/signin.html')
+    
+def signout(request):
+    logout(request)
+    return redirect('signin')
