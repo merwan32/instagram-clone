edff77a626f8e33a5c0ebf3c2df4dcb293396699
merwan bekkar <Bkar.mrwan32@gmail.com>
2022-08-10 22:42:18 +0200

    add search page

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns=[
     path('',views.index,name="index"),
     path('profile/',views.profile,name="profile"),
+    path('search/',views.search,name="search"),
+
+    
     path('signup/',views.signup,name="signup"),
     path('signin/',views.signin,name="signin"),
     path('signout/',views.signout,name="signout"),
