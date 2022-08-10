48f0305168915f0c02f8c3ee740600f2c877bdf1
merwan bekkar <Bkar.mrwan32@gmail.com>
2022-08-11 01:30:23 +0200

    add following system

--- app/views.py
+++ app/views.py
@@ -17,7 +17,28 @@ def search(request):
     profile = Profile.objects.get(user=request.user)
     search = request.GET['username']
     profiles = Profile.objects.filter(user__username__icontains=search)
-    return render(request,'search.html',{'profile':profile,'profiles':profiles})
+    prof1,prof2,prof3 = set(),set(),set()
+    for p in profiles:
+        if p.user == request.user :
+            prof1.add(p)
+        elif request.user in p.followers.all():
+            prof2.add(p)
+        else:
+            prof3.add(p)
+    return render(request,'search.html',{'profile':profile,'prof1':prof1,'prof2':prof2,'prof3':prof3,"username":search})
+
+def follow(request,id,username):
+    profile = Profile.objects.get(id=id)
+    login_profile = Profile.objects.get(user=request.user)
+    if request.user in profile.followers.all():
+        profile.followers.remove(request.user)  
+        login_profile.followings.remove(profile.user)
+    else:
+        profile.followers.add(request.user)  
+        login_profile.followings.add(profile.user)
+    return redirect(f'/search?username={username}')
+
+
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -31,8 +52,9 @@ def signup(request):
             user = User.objects.create_user(username=username,email=email,password=password)
             profil = Profile.objects.create(user=user)
             if profil :
-                messages.success(request,'profile created please login')
-                return redirect("signin")
+                user = authenticate(username=username,password=password)
+                login(request,user)
+                return redirect("index")
     return render(request,'auth/signup.html')
 
 def signin(request):
@@ -42,7 +64,7 @@ def signin(request):
         user = authenticate(username=username,password=password)
         if user:
             login(request,user)
-            return redirect("profile")
+            return redirect("index")
     return render(request,'auth/signin.html')
     
 def signout(request):
